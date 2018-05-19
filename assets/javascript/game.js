92f2308cdfe2df4fcf868a7d7040e89c41205f80
Michael Schumann <schumann82@gmail.com>
2018-05-18 17:18:36 -0700

    mostly done but throwing error

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -19,6 +19,8 @@ function randomNumber () { //create a function that creates a random number
     
     $("#rndNum").text(rndNumber);
     $("#score").text(userScore);
+    $("#losses").text(losses);
+    $("#wins").text(wins);
 
     var earth = randomNumber(); 
     console.log(earth);
@@ -32,6 +34,37 @@ function randomNumber () { //create a function that creates a random number
     var wind = randomNumber(); 
     console.log(wind);
 
+    function reset() {
+        randomNumber();
+        randomTotal();
+        userScore = 0;
+        var earth = randomNumber(); 
+        console.log(earth);
+
+        var fire = randomNumber(); 
+        console.log(fire);
+
+        var water = randomNumber(); 
+        console.log(water);
+
+        var wind = randomNumber(); 
+        console.log(wind);
+
+
+    }
+
+    function wins() {
+        wins++; 
+        $("#wins").text(wins);
+        reset();
+    }
+
+    function losses() {
+        wins++; 
+        $("#losses").text(losses);
+        reset();
+    }
+
     
 
         
@@ -40,29 +73,63 @@ function randomNumber () { //create a function that creates a random number
         console.log(earth);
         console.log(userScore);
         $("#score").text(userScore);
+        if (userScore === rndNumber) {
+            wins();
+            
+        }
+        if (userScore > rndNumber) {
+            losses();
+            
+        }
             
     });
 
-    $("#fire").on("click", function () { //for homework we could call the function
+    $("#fire").on("click", function () { 
         userScore = userScore + fire;
         console.log(fire);
         console.log(userScore);
         $("#score").text(userScore);
+        if (userScore === rndNumber) {
+            wins();
+            
+        }
+        if (userScore > rndNumber) {
+            losses();
+            
+        }
     });
 
-    $("#water").on("click", function () { //for homework we could call the function
+    $("#water").on("click", function () { 
         userScore = userScore + water;
         console.log(water);
         console.log(userScore);
         $("#score").text(userScore);
+        if (userScore === rndNumber) {
+            wins();
+            
+        }
+        if (userScore > rndNumber) {
+            losses();
+            
+        }
     });
 
-    $("#wind").on("click", function () { //for homework we could call the function
+    $("#wind").on("click", function () { 
         userScore = userScore + wind;
         console.log(wind);
         console.log(userScore);
         $("#score").text(userScore);
+        if (userScore === rndNumber) {
+            wins();
+            
+        }
+        if (userScore > rndNumber) {
+            losses();
+            
+        }
     });
+
+    
 
 
 
